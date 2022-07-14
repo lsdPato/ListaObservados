d8cdba107a56af5b095ebdc975a36a8feb22f633
lsdPato <patoche152@gmail.com>
2022-07-13 20:55:50 -0500

    Initial commit

---
+++ src/main/java/ec/edu/arquitectura/sistema2/Sistema2Application.java
@@ -0,0 +1,13 @@
+package ec.edu.arquitectura.sistema2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sistema2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sistema2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/arquitectura/sistema2/Sistema2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.arquitectura.sistema2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sistema2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
